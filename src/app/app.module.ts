30e61103adb54401e880f2175ebc5e55c9cce446
hp <rmaru470@gmail.com>
2020-09-10 07:21:33 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatTabsComponent } from './mat-tabs/mat-tabs.component';
-import { MatTabsModule,MatSelectModule,MatCheckboxModule,MatButtonModule,MatFormFieldModule,MatInputModule,MatIconModule} from '@angular/material';
+import { MatTabsModule,MatSelectModule,MatCheckboxModule,MatButtonModule,MatFormFieldModule,MatInputModule,MatIconModule,MatTableModule} from '@angular/material';
 import { TableComponent } from './table/table.component';
 import { HeaderButtonsComponent } from './header-buttons/header-buttons.component';
 import { HeadingScreenComponent } from './heading-screen/heading-screen.component'
@@ -25,7 +24,8 @@ import { HeadingScreenComponent } from './heading-screen/heading-screen.componen
     MatButtonModule,
     MatFormFieldModule,
     MatInputModule,
-    MatIconModule
+    MatIconModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,12 +1,38 @@
 import { Component, OnInit} from '@angular/core';
+import {MatTableDataSource} from '@angular/material';
+export interface PeriodicElement {
+  custref: string;
+  mid: number;
+  merchant_name:string;
+  currency:string;
+  valid_from:string;
+  valid_to:string;
+}
 
+const ELEMENT_DATA: PeriodicElement[] = [
+  {custref: 'PO1674783569 ', mid: 11090654, merchant_name: 'ABC', currency: 'GBP',valid_from:'2017',valid_to:'2020'},
+  {custref: 'PO1774783569 ', mid: 90090654, merchant_name: 'DEF', currency: 'GSP',valid_from:'2017',valid_to:'2020'},
+  {custref: 'PO1874783569 ', mid: 80090654, merchant_name: 'GHI', currency: 'KBP',valid_from:'2017',valid_to:'2020'},
+  {custref: 'PO1974783569 ', mid: 70090654, merchant_name: 'JKL', currency: 'TBP',valid_from:'2017',valid_to:'2020'},
+  {custref: 'PO2074783569 ', mid: 60090654, merchant_name: 'MNO', currency: 'HBP',valid_from:'2017',valid_to:'2020'},
+  {custref: 'PO2174783569 ', mid: 50090654, merchant_name: 'PQR', currency: 'GBL',valid_from:'2017',valid_to:'2020'},
+  {custref: 'PO2274783569 ', mid: 10090654, merchant_name: 'STU', currency: 'MTP',valid_from:'2017',valid_to:'2020'},
+  {custref: 'PO2374783569 ', mid: 20090654, merchant_name: 'VWX', currency: 'STP',valid_from:'2017',valid_to:'2020'},
+  {custref: 'PO2474783569 ', mid: 30090654, merchant_name: 'YZA', currency: 'FTP',valid_from:'2017',valid_to:'2020'},
+  {custref: 'PO2574783569 ', mid: 40090654, merchant_name: 'BCD', currency: 'JBL',valid_from:'2017',valid_to:'2020'},
+];
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.css']
 })
 export class TableComponent implements OnInit {
+  displayedColumns: string[] = ['custref', 'mid', 'lcp', 'merchant_name','interchange_country','card_name','processing_agent','acpt_commercial_card','refund','currency','valid_from','valid_to'];
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
 
+  applyFilter(filterValue:string){
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
   constructor() { }
 
   ngOnInit() {
